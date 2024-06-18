297bb83f6cb805974a4579eae409cab86e7a5767
Chin Isradisaikul <chinawat.i@cmu.ac.th>
2024-06-18 08:36:59 +0700

    finished intro

--- Frap/Intro.lean
+++ Frap/Intro.lean
@@ -42,10 +42,86 @@ example (p q : Prop) : p → q := by
 ## Proving implications
 
 The left-hand side of an implication is an assumption that may be used in the proof.
-To introduce the assumption given by an implication, use the `intro` tactic.
+To introduce the assumption given by an implication, use the `intro` tactic, followed by the name of the assumption being introduced.
 
+To use an assumption, use the `exact` tactic, followed by the name of the assumption to be used.
+If the provided assumption cannot be used in the current context, an error occurs.
 -/
 
 theorem identity (p : Prop) : p → p := by
   intro hp
   exact hp
+
+/-
+If we have an implication in assumptions and want to use it, we can use the `apply` tactic with the name of the assumption.
+
+In fact, `exact` is a more efficient variant of `apply`.
+-/
+
+theorem modus_ponens (p q : Prop) : p -> (p -> q) -> q := by
+  intro hp
+  intro h
+  apply h
+  exact hp
+
+/-
+## Conjunctions
+
+Conjunction symbol (∧) can be typed using `\and` shortcut.
+
+## Proving conjunctions
+
+When the proof goal is a conjunction, we need to show that each side of the conjunction holds.
+Here, we can apply the `And.intro` tactic to break the conjunction down into two cases.
+We give the proof of each case in turn.
+-/
+
+theorem and_intro (p q : Prop) : p -> q -> p ∧ q := by
+  intro hp
+  intro hq
+  apply And.intro
+  exact hp
+  exact hq
+
+/-
+When we have multiple subgoals to work with, it might be nice to focus on one goal at a time.  Use the `.` tactic to select the first subgoal (and ignore the rest for now).
+-/
+example (p q : Prop) : p -> q -> p ∧ q := by
+  intro hp
+  intro hq
+  apply And.intro
+  . exact hp
+  . exact hq
+
+/-
+If we have a conjunction in assumptions and want to use it, we can break it down into individual assumptions using the `cases` tactic on such assumption.
+In this situation, there is only one case to work with (namely, the conjunction itself), but within it, there are two smaller assumptions.
+We can use the `intro` tactic to introduce each one.
+-/
+theorem and_elim_l (p q : Prop) : p ∧ q → p := by
+  intro hand
+  cases hand with
+  | intro hp hq => exact hp
+
+/-
+exercise (1-star)
+-/
+theorem and_elim_r (p q : Prop) : p ∧ q → p := by
+  sorry
+
+/-
+Alternatively, if we only need to use one side of the conjunction, we can apply the `And.left` or `And.right` tactic as appropriate.
+
+In this example, after applying `And.left`, two subgoals result, with a placeholder variable `?b` being introduced.
+Applying the assumption _unifies_ the placeholder with the right-hand side of the conjunction.
+-/
+theorem and_elim_l' (p q : Prop) : p ∧ q → p := by
+  intro hand
+  apply And.left
+  apply hand
+/-
+In the example above, we already know which assumption we want to use `And.left` on, so we can provide the assumption for the `apply` tactic to work with immediately.
+-/
+example (p q : Prop) : p ∧ q → p := by
+  intro hand
+  apply And.left hand
